e7e104e84e186389e271323158d33b7416b4836d
Rai Angelo <raizereangelo88@gmail.com>
2025-12-06 15:01:19 +0800

    Add files via upload

---
+++ activity 22.py
@@ -0,0 +1,15 @@
+months=['jan','feb','mar','apr','may','jun','jul']
+
+print(months)
+
+months.reverse()
+print(months)
+
+fullname= 'raizere angelo ganac'
+
+newlist= list(fullname)
+newlist.reverse()
+print(newlist)
+
+newlist.sort()
+print(newlist)
